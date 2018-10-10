668952cf6dcff05d8cb7898f72463c9213ffbc46
CreativemindOne <space.aminis@gmail.com>
2018-10-09 21:19:22 -0700

    Finsl commit Oct 9

--- FirstProject/src/com/test/DisplayName.java
+++ FirstProject/src/com/test/DisplayName.java
@@ -5,7 +5,8 @@ public class DisplayName {
 	public static void main(String[] args) {
 		System.out.println("Hi, Welcome!");
 		System.out.println("Hope this works");
-		System.out.prinln("Edit inside a branch from repo");
+		System.out.println("Edit inside a branch from repo");
+		System.out.println("pushing from pc for final commit Oct 9th");
 
 	}
 
