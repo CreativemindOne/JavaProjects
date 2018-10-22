7e3527bd81407eeb06f4adb3a3f3c6b74761b29b
CreativemindOne <space.aminis@gmail.com>
2018-10-21 19:36:25 -0700

    welcomeUserFirstCommit

---
+++ FirstProject/src/com/test/WelcomeUser.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class WelcomeUser {
+
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter your Name");
+		String name=userInput.next();
+		System.out.println("Hi git");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
